14d288466b2eea25dc6f699447dbcc37dcfa3e0d
IlyaBalykov <balykov.ilya.88@gmail.com>
2021-05-30 21:59:33 +0300

    Let there be music

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 document.addEventListener("keydown", function (e) {
     if (e.code == "KeyA") {
+        let Key = document.createElement("AUDIO");
+        Key.src = "media/A.mp3";
+        Key.play();
         console.log("The \'A\' key is pressed");
     }
     else if (e.code == "KeyS") {
+        let Key = document.createElement("AUDIO");
+        Key.src = "media/S.mp3";
+        Key.play();
         console.log("The \'S\' key is pressed");
     }
     else if (e.code == "KeyD") {
+        let Key = document.createElement("AUDIO");
+        Key.src="media/D.mp3";
+        Key.play();
         console.log("The \'D\' key is pressed");
     }
     else if (e.code == "KeyF") {
+        let Key = document.createElement("AUDIO");
+        Key.src="media/F.mp3";
+        Key.play();
         console.log("The \'F\' key is pressed");
     }
     else if (e.code == "KeyG") {
+        let Key = document.createElement("AUDIO");
+        Key.src="media/G.mp3";
+        Key.play();
         console.log("The \'G\' key is pressed");
     }
     else if (e.code == "KeyH") {
+        let Key = document.createElement("AUDIO");
+        Key.src="media/H.mp3";
+        Key.play();
         console.log("The \'H\' key is pressed");
     }
     else if (e.code == "KeyJ") {
+        let Key = document.createElement("AUDIO");
+        Key.src="media/J.mp3";
+        Key.play();
         console.log("The \'J\' key is pressed");
     }
     else {
